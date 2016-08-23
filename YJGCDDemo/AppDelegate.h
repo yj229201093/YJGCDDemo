0d9b4429dc3de32ba9652c64792a3d2937552b30
YangJian <229201093@qq.com>
2016-08-23 15:48:33 +0800

    Initial Commit

---
+++ YJGCDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YJGCDDemo
+//
+//  Created by GongHui_YJ on 16/8/23.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YJGCDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YJGCDDemo
+//
+//  Created by GongHui_YJ on 16/8/23.
+//  Copyright © 2016年 YangJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
